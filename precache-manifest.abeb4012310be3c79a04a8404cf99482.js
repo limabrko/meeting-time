18492afe470940f7529ea56056883615c42f938e
Felipe Lima <talktofill@gmail.com>
2019-01-01 16:17:04 +0900

    build generated

---
+++ precache-manifest.abeb4012310be3c79a04a8404cf99482.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d945121e25902d2080fe",
+    "url": "/static/css/main.4081ce61.chunk.css"
+  },
+  {
+    "revision": "d945121e25902d2080fe",
+    "url": "/static/js/main.d945121e.chunk.js"
+  },
+  {
+    "revision": "ef0ebc799dad26893dc8",
+    "url": "/static/css/1.1481c047.chunk.css"
+  },
+  {
+    "revision": "ef0ebc799dad26893dc8",
+    "url": "/static/js/1.ef0ebc79.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "23b52c40ad0cebd49edd39e1366c9145",
+    "url": "/index.html"
+  }
+];
